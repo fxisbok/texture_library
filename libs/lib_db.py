68c21989ff3509fb551a58f7be06e8b3bb71a2d7
fxisbok99 <fx.isbok99@gmail.com>
2025-07-26 12:31:16 +0900

    CRUD prototype

--- libs/lib_db.py
+++ libs/lib_db.py
@@ -1,3 +1,4 @@
+import typing
 import pymysql
 
 class OpenDB:
@@ -16,6 +17,53 @@ class OpenDB:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.cursor.execute('COMMIT')
         self.cursor.close()
+
+class CRUD:
+    def get_query_insert(self, table: str, fields: str="*", vals: str = ""):
+        """
+        C for CRUD. combine query command based on arguments.
+        :param table: data table name
+        :param fields: column to read
+        :param vals: input values
+        :return: combined query command
+        """
+        result = f"INSERT INTO {table} {fields} VALUES {vals}"
+        return result
+
+    def get_query_select(self, table: str, fields: str="*", filters: str = ""):
+        """
+        R for CRUD. combine query command based on arguments.
+        :param table: data table name
+        :param fields: column to read
+        :param filters: filter for search
+        :return: combined query command
+        """
+        result = f"SELECT {fields} FROM {table}"
+        if filters:
+            result += f" WHERE {filters}"
+        return result
+
+    def get_query_update(self, table: str, fields: str="*", filters: str = ""):
+        """
+        U for CRUD. combine query command based on arguments.
+        :param table: data table name
+        :param fields: column to read
+        :param filters: filter for search
+        :return: combined query command
+        """
+        result = f"UPDATE {table} SET {fields} WHERE {filters}"
+        return result
+
+    def get_query_delete(table: str, filters: str = ""):
+        """
+        D for CRUD. combine query command based on arguments.
+        :param table: data table name
+        :param filters: filter for search
+        :return: combined query command
+        """
+        result = f"DELETE FROM {table} WHERE {filters}"
+        return result
+
 
 if __name__ == "__main__":
     with OpenDB() as database:
